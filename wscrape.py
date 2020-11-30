5f2a8f9588e4472c5d3ea9fe7165af484a0f1c42
Jakob Lindén <jakob-wow@live.se>
2020-11-30 21:04:43 +0100

    pushman

--- wscrape.py
+++ wscrape.py
@@ -1,31 +1,35 @@
 import os
+import time
 from twilio.rest import Client
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 
-# Send message
-account_sid = 'ACaeee2b5673a49dd89e983783e3a05b19'
-auth_token = '9c85117ecb761a410802b856a8e376ce'
 
-client = Client(account_sid, auth_token)
+def wscrape():
+    # Send message
+    account_sid = 'ACaeee2b5673a49dd89e983783e3a05b19'
+    auth_token = '9c85117ecb761a410802b856a8e376ce'
 
-# Scrape gambling cabin
-url = 'https://gamblingcabin.se/andelar-spela-tillsammans/'
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    client = Client(account_sid, auth_token)
 
-webpage = urlopen(req).read()
-page_soup = soup(webpage, 'html.parser')
+    # Scrape gambling cabin
+    url = 'https://gamblingcabin.se/andelar-spela-tillsammans/'
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-title = page_soup.find()
+    webpage = urlopen(req).read()
+    page_soup = soup(webpage, 'html.parser')
 
-containers = page_soup.findAll('span','elementor-button-text')
+    containers = page_soup.findAll('span','elementor-button-text')
 
-for container in containers:
-    if 'Bengan' or 'Dybban' or 'Giganten' or 'Vichyn' or 'Timman' in str(container):
-        client.messages.create(
-            to='+46760471200',
-            from_='+12513094758',
-            body='GTO-andelar uppe: https://gamblingcabin.se/andelar-spela-tillsammans/'
-        )
-        break
+    for container in containers:
+        if 'Bengan' or 'Dybban' or 'Giganten' or 'Vichyn' or 'Timman' or 'Rejken' in str(container):
+            client.messages.create(
+                to='+46760471200',
+                from_='+12513094758',
+                body='GTO-andelar uppe: https://gamblingcabin.se/andelar-spela-tillsammans/'
+            )
+            break
 
+while True:
+    wscrape()
+    time.sleep(1800)
