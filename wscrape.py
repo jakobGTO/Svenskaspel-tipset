095f7a4d2e29330be5e0c1765d9b7fe9baab05a9
Jakob Lindén <jakob-wow@live.se>
2020-11-30 19:41:44 +0100

    pushman

---
+++ wscrape.py
@@ -0,0 +1,31 @@
+import os
+from twilio.rest import Client
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+# Send message
+account_sid = 'ACaeee2b5673a49dd89e983783e3a05b19'
+auth_token = '9c85117ecb761a410802b856a8e376ce'
+
+client = Client(account_sid, auth_token)
+
+# Scrape gambling cabin
+url = 'https://gamblingcabin.se/andelar-spela-tillsammans/'
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+webpage = urlopen(req).read()
+page_soup = soup(webpage, 'html.parser')
+
+title = page_soup.find()
+
+containers = page_soup.findAll('span','elementor-button-text')
+
+for container in containers:
+    if 'Bengan' or 'Dybban' or 'Giganten' or 'Vichyn' or 'Timman' in str(container):
+        client.messages.create(
+            to='+46760471200',
+            from_='+12513094758',
+            body='GTO-andelar uppe: https://gamblingcabin.se/andelar-spela-tillsammans/'
+        )
+        break
+
